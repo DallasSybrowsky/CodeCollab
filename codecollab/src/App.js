e44a44bdb70d1e064c0c66edaef05c535129dfdf
Dallas Sybrowsky <dallas.sybrowsky@gmail.com>
2023-03-08 21:56:26 -0800

    Initial file upload

---
+++ codecollab/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          CodeCollab Homepage
+        </h1>
+        <p>
+          Find some code Homies!
+        </p>
+        <a>Home</a>
+        <a>Home</a>
+        <a>Home</a>
+        <a>Home</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
