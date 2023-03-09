1cd266c076244be0c27440b1110459930b7115ca
ana <ana@anniecannons.com>
2023-03-09 11:41:53 -0800

    added files

--- codecollab/src/App.js
+++ codecollab/src/App.js
@@ -1,20 +1,18 @@
+import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          CodeCollab Homepage
-        </h1>
-        <p>
-          Find some code Homies!
-        </p>
-        <a>Home</a>
-        <a>Home</a>
-        <a>Home</a>
-        <a>Home</a>
-      </header>
+      <Nav />
+<Footer />
     </div>
   );
 }
