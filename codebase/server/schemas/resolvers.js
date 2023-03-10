35873b51fced4cde78f959febc287f7516d20a20
Risvi-Tareq <ivsirqerat@gmail.com>
2023-03-09 21:37:24 -0800

    add friends added to schema. typedefs and resolvers need work

--- codebase/server/schemas/resolvers.js
+++ codebase/server/schemas/resolvers.js
@@ -1,15 +1,21 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Project } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Project } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   // fetches the following data to be loaded onto the page
   Query: {
     users: async () => {
-      return User.find().populate('projects');
+      return User.find().populate("projects");
     },
     user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('projects');
+      return User.findOne({ username }).populate("projects");
+    },
+    friends: async () => {
+      return User.find().populate("friends");
+    },
+    friend: async () => {
+      return User.findOne({ username }).populate("projects");
     },
     projects: async (parent, { username }) => {
       const params = username ? { username } : {};
@@ -20,9 +26,9 @@ const resolvers = {
     },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('projects');
+        return User.findOne({ _id: context.user._id }).populate("projects");
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 
@@ -37,13 +43,13 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+        throw new AuthenticationError("No user found with this email address");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
@@ -65,7 +71,7 @@ const resolvers = {
 
         return project;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
     addComment: async (parent, { projectId, commentText }, context) => {
       if (context.user) {
@@ -82,7 +88,7 @@ const resolvers = {
           }
         );
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
     removeProject: async (parent, { projectId }, context) => {
       if (context.user) {
@@ -98,7 +104,7 @@ const resolvers = {
 
         return project;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
     removeComment: async (parent, { projectId, commentId }, context) => {
       if (context.user) {
@@ -115,7 +121,7 @@ const resolvers = {
           { new: true }
         );
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
