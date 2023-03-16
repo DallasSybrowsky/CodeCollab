5acd10ac4269502921584976ce4a92aaa2099691
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2023-03-15 20:06:21 -0700

    Add mutations and queries for user projects.

--- codebase/server/models/Project.js
+++ codebase/server/models/Project.js
@@ -9,7 +9,7 @@ const projectSchema = new Schema({
     maxlength: 280,
     trim: true,
   },
-  projectDescription: {
+  projectText: {
     type: String,
     required: 'You need to leave a description!',
     minlength: 1,
