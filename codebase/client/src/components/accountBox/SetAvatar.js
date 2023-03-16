5057a5ae49c52e28039e2af9b2bf045d82f92e20
ana <ana@anniecannons.com>
2023-03-15 20:02:02 -0700

    added avatar

--- codebase/client/src/components/accountBox/SetAvatar.js
+++ codebase/client/src/components/accountBox/SetAvatar.js
@@ -1,24 +1,128 @@
 import React from "react";
 import styled from "styled-components";
-import {useState, useEffect} from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
-import {setAvatarRoute} from "../accountBox/APIRoutes";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+
+
 // import loader from "../../assets/loader.gif";
-const Container = styled.div`
+
+const AvatarContainer = styled.div`
+  color:#FFC442;
+  font-family: "Courier Prime", monospace;
+  font-size: 1rem;
+ 
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: nowrap;
+  gap: 6em;
+
+
+  gap: 1em;
+  
+  justify-content: center;
+
 `;
-export default function SetAvatar() {
-const api = 'https://api.multiavatar.com/45678945';
-const navigate = useNavigate();
+
+
+
+
+
+
+
+
+
+
+
+// function SetAvatar() {
+//   const [avatars, setAvatars] = useState([]);
+//   const [selectedAvatar, setSelectedAvatar] = useState(undefined);
+
+//   useEffect(() => {
+//     const fetchAvatars = async () => {
+//       try {
+//         const response = await fetch('https://api.multiavatar.com/');
+//         const avatarIds = await response.json();
+
+//         const avatarPromises = avatarIds.map(async id => {
+//           const avatarResponse = await fetch(`https://api.multiavatar.com/${id}`);
+//           return avatarResponse.text();
+//         });
+
+//         const avatarData = await Promise.all(avatarPromises);
+//         setAvatars(avatarData);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchAvatars();
+//   }, []);
+
+//   const handleAvatarClick = index => {
+//     setSelectedAvatar(index);
+//     // Set the selected avatar as the user's profile avatar
+//   };
+
+//   return (
+//     <AvatarContainer>
+//     <div>
+//       <div className="title-container">
+//         <h1>Please pick your Avatar</h1>
+//       </div>
+//       <div className="avatars">
+//         {avatars.map((avatar, index) => (
+//           <div
+//             key={index}
+//             className={`avatar ${selectedAvatar === index ? 'selected' : ''}`}
+//             onClick={() => handleAvatarClick(index)}
+//           >
+//             <img
+//               src={`data:image/svg+xml;base64,${(avatar)}`}
+//               alt={`Avatar ${index}`}
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//     </AvatarContainer>
+//   );
+// }
+
+// export default SetAvatar;
+
+
+
+
+
+function SetAvatar() {
+  
+  
+  const api = 'https://api.multiavatar.com/';
+  const avatarIds = ['333', '26839', '296547', '8647586', '759384', '394856'];
+
+  const [avatarData, setAvatarData] = useState({});
+
+  useEffect(() => {
+    avatarIds.forEach(avatarId => {
+      fetch(api + avatarId)
+        .then(res => res.text())
+        .then(svg => setAvatarData(prevAvatarData => ({
+          ...prevAvatarData,
+          [avatarId]: svg
+        })));
+    });
+  }, []);
+
   return (
-   <>
-   <Container >
-    <div>
+    <AvatarContainer>
+       <div className="title-container">
+       <h1> Please pick your  Avatar</h1>
     </div>
-   </Container>
-   <ToastContainer />
-   </>
+    <div>
+      {avatarIds.map(avatarId => (
+        <div key={avatarId} dangerouslySetInnerHTML={{__html: avatarData[avatarId]}} />
+      ))}
+    </div>
+    </AvatarContainer>
   );
 }
+ export default SetAvatar;
