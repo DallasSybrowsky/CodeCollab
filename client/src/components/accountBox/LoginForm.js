187e6378254c6bd0d880738ad2f45d42a7400471
Dallas Sybrowsky <dallas.sybrowsky@gmail.com>
2023-03-16 21:46:06 -0700

    HUGE restructure

--- client/src/components/accountBox/LoginForm.js
+++ client/src/components/accountBox/LoginForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useContext } from "react";
-import { AccountContext } from "../../components/accountBox/AccountContext";
+import { AccountContext } from "./AccountContext";
 import {
   BoldLink,
   BoxContainer,
@@ -8,16 +8,15 @@ import {
   Input,
   MutedLink,
   SubmitButton,
-} from "../accountBox/Common";
+} from "./Common";
 
 import AuthService from "../../utils/auth";
 
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../../utils/mutations";
 
-
 export function LoginForm(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -47,8 +46,8 @@ export function LoginForm(props) {
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
@@ -64,9 +63,9 @@ export function LoginForm(props) {
           type="email"
           placeholder="Email"
           name="email"
-          value={formState.email} 
+          value={formState.email}
           onChange={handleChange}
-          />
+        />
         <Input
           id="login-input2"
           type="password"
@@ -74,14 +73,15 @@ export function LoginForm(props) {
           name="password"
           value={formState.password}
           onChange={handleChange}
-           />
-        <SubmitButton id="login-button" type="submit">Sign In</SubmitButton>
+        />
+        <SubmitButton id="login-button" type="submit">
+          Sign In
+        </SubmitButton>
       </FormContainer>
 
       <MutedLink id="muted-link" href="#">
         Forget your password?
       </MutedLink>
-
 
       <MutedLink id=" muted-link" href="#">
         Don't have an account?
