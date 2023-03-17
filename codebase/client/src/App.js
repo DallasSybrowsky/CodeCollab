9d2952724fb40ca216966c709049d04d257220ac
Risvi-Tareq <ivsirqerat@gmail.com>
2023-03-16 20:09:31 -0700

    compiled with Anas code

--- codebase/client/src/App.js
+++ codebase/client/src/App.js
@@ -11,23 +11,17 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Chat from "./components/accountBox/Chat";
-
 import styled from "styled-components";
-
 import { io } from "socket.io-client";
 import SetAvatar from "./components/accountBox/SetAvatar";
 import CardExample from "./components/accountBox/Card";
-
 import ExploreCard from "./components/accountBox/ExploreCard";
 import About from "./components/accountBox/About";
 import ProfileCard from "./components/accountBox/ProfileCard";
-
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
-
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem("id_token");
@@ -39,13 +33,11 @@ const authLink = setContext((_, { headers }) => {
     },
   };
 });
-
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
 const AppContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -54,13 +46,11 @@ const AppContainer = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
 const URL =
   process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
 export const socket = io(URL, {
   autoConnect: false,
 });
-
 function App() {
   const [input, setInput] = useState("");
   const handleChange = (e) => {
@@ -74,19 +64,14 @@ function App() {
       setInput("");
     }
   }
-//  this is for the profile cards 
-
+//  this is for the profile cards
 const [cards, setCards] = useState([
   {
-    
     title: 'What I learned from my visit to The Upside Down',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
     author: 'Nancy Wheeler'
   }
- 
 ]);
- 
-
   return (
     <ApolloProvider client={client}>
       {/* Beginning of chat test functionality */}
@@ -117,40 +102,42 @@ const [cards, setCards] = useState([
             element={
               <>
                 <Nav />
-               
                   <ExploreCard
                     key={ExploreCard.id}
                     projectTitle={ExploreCard.projectTitle}
                     projectAuthor={ExploreCard.projectAuthor}
                   />
-               
                 <Footer />
               </>
             }
           />
-          
-          
+          <Route
+            path="/about"
+            element={
+              <>
+                <Nav />
+                <About />
+                <Footer />
+              </>
+            }
+          />
           <Route
             path="/profile"
             element={
               <>
                 <Nav />
-                <ProfileCard 
+                <ProfileCard
                  key={ProfileCard.id}
                  projectTitle={ProfileCard.projectTitle}
                  projectDescription={ProfileCard.projectDescription}
                  />
-              
                 <Footer />
               </>
             }
           />
-          
-          
         </Routes>
       </Router>
     </ApolloProvider>
   );
 }
-
 export default App;
